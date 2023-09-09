b17e2df0b736680c200984ddbfba3f0500d0ced5
Nihar Reddy <niharreddy3@gmail.com>
2023-09-08 21:43:18 -0500

    ui additions

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+let playerScore = 0;
+document.querySelector('#player-score').innerHTML = `Player Score: ${playerScore}`;
+
+
+let computerScore = 0;
+document.querySelector('#computer-score').innerHTML = `Computer Score: ${computerScore}`;
+
+let gamesPlayed = 0;
+document.querySelector('#games-played').innerHTML = `Games Played: ${gamesPlayed}`;
+
+let computerSelection = '';
+let currentRoundResults = '';
+let winner = ''; 
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', () => {
+
+    gamesPlayed += 1;
+    document.querySelector('#games-played').innerHTML = `Games Played: ${gamesPlayed}`;
+    playRound(button.textContent, getComputerChoice())
+
+}))
+
 function getComputerChoice() {
 
     // return a random int from 0 to 2 
@@ -13,48 +38,53 @@ function getComputerChoice() {
 
 }
 
-function playRound(playersChoice, computersChoice) {
+function playRound(playerSelection, computerSelection) {
 
-    let playersChoiceLowerCase = playersChoice.toLowerCase();
+    // check scores for 5 
+
+    document.querySelector('#computer-selection').innerHTML = `${computerSelection}`;
+
+    let playerSelectionLowerCase = playerSelection.toLowerCase();
 
     // case where they are the same 
-    if (computersChoice === playersChoiceLowerCase) {
-        return 'tie game'
+    if (computerSelection === playerSelectionLowerCase) {
+        document.querySelector('#current-round-results').innerHTML = 'tie game, select another choice';
     }
 
-    if (playersChoiceLowerCase === 'rock') {
-        if (computersChoice === 'scissors') {
-            return 'You win! Rock beats Scissors';
-        } else if (computersChoice === 'paper') {
-            return 'You lose! Paper beats Rock'
+    if (playerSelectionLowerCase === 'rock') {
+        if (computerSelection === 'scissors') {
+            playerScore += 1;
+            document.querySelector('#player-score').innerHTML = `Player Score: ${playerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You win! Rock beats Scissors';
+        } else if (computerSelection === 'paper') {
+            computerScore += 1;
+            document.querySelector('#computer-score').innerHTML = `Computer Score: ${computerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You lose! Paper beats Rock';
         }
-    } else if (playersChoiceLowerCase === 'paper') {
-        if (computersChoice === 'rock') {
-            return 'You win! Paper beats Rock';
-        } else if (computersChoice === 'scissors') {
-            return 'You lose! Scissors beats Paper'
+    } else if (playerSelectionLowerCase === 'paper') {
+        if (computerSelection === 'rock') {
+            playerScore += 1;
+            document.querySelector('#player-score').innerHTML = `Player Score: ${playerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You win! Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            computerScore += 1;
+            document.querySelector('#computer-score').innerHTML = `Computer Score: ${computerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You lose! Scissors beats Paper';
         }
-    } else if (playersChoiceLowerCase === 'scissors') {
-        if (computersChoice === 'paper') {
-            return 'You win! Scissors beats paper';
-        } else if (computersChoice === 'rock') {
-            return 'You lose! Rock beats Scissors'
+    } else if (playerSelectionLowerCase === 'scissors') {
+        if (computerSelection === 'paper') {
+            playerScore += 1;
+            document.querySelector('#player-score').innerHTML = `Player Score: ${playerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You win! Scissors beats paper';
+        } else if (computerSelection === 'rock') {
+            computerScore += 1;
+            document.querySelector('#computer-score').innerHTML = `Computer Score: ${computerScore}`;
+            document.querySelector('#current-round-results').innerHTML = 'You lose! Rock beats Scissors';
         }
     }
 
 }
 
-function game() {
-
-    for (let i = 0; i <= 5; i++) {
-        let playerSelection = prompt('What is player choice?')
-        let computersChoice = getComputerChoice()
-        console.log(`Player choice was: ${playerSelection}`)
-        console.log(`Computer choice was: ${computersChoice}`)
-        console.log(playRound(playerSelection, computersChoice));
-    }
-
-}
 
 
 
